7a5b11792d9f54208019886e58d7e1a227519a17
haorang <haorang@berkeley.edu>
2019-01-21 16:50:21 -0800

    ho

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+import './App.css'
+
+function Header() {
+    return (
+        <header className = "Header">
+            Haoran's Hospitable Haberdashery
+        </header>
+    )
+}
+
+export default Header
